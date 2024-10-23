7bad20ef16d6f5e26a5c644bfc96c9e9fe589b84
nihal-ranchod <nihal.ranchod@gmail.com>
2024-10-23 12:05:46 +0200

    increased training params

--- genetic_algorithm_train.py
+++ genetic_algorithm_train.py
@@ -2,22 +2,22 @@ import os
 from genetic_algorithm import GeneticChessBot
 
 def train_standard_bot():
-    bot = GeneticChessBot(population_size=50)
+    bot = GeneticChessBot(population_size=100)
     bot.train_on_pgn(
         pgn_file="PGN_Data/lichess_db_standard_rated_2013-01.pgn",
-        num_generations=1000,
-        games_per_genome=100
+        num_generations=2500,
+        games_per_genome=500
     )
     bot.save_best_genome("best_genome.pkl")
     bot.plot_learning_progress("Standard PGN Data")
     bot.save_fitness_history_to_csv("ga_fitness_history_standard_pgn.csv")
 
 def train_puzzle_bot():
-    bot = GeneticChessBot(population_size=50)
+    bot = GeneticChessBot(population_size=100)
     bot.train_on_puzzles(
         puzzle_pgn_file="PGN_Data/lichess_db_puzzle_converted.pgn",
-        num_generations=1000,
-        puzzles_per_genome=100
+        num_generations=2500,
+        puzzles_per_genome=500
     )
     bot.save_best_genome("best_genome_puzzle.pkl")
     bot.plot_learning_progress("Puzzle PGN Data")
